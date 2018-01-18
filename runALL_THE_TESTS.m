7aba1f0a72c2a4ed18c896c3d297b3473ff48677
Andy Campbell <ranford.b.crandal@gmail.com>
2018-01-18 17:09:21 -0500

    Create runALL_THE_TESTS.m

---
+++ runALL_THE_TESTS.m
@@ -0,0 +1,35 @@
+try
+    import('matlab.unittest.TestRunner');
+    import('matlab.unittest.plugins.XMLPlugin');
+    import('matlab.unittest.plugins.ToFile');
+    import('matlab.unittest.plugins.CodeCoveragePlugin');
+    import('matlab.unittest.plugins.codecoverage.CoberturaFormat');
+
+    ws = getenv('WORKSPACE');
+    
+    src = fullfile(ws, 'source');
+    addpath(src);
+    
+    tests = fullfile(ws, 'tests');
+    suite = testsuite(tests);
+
+    % Create and configure the runner
+    runner = TestRunner.withTextOutput('Verbosity',3);
+
+    % Add the XML plugin
+    resultsDir = fullfile(ws, 'testresults');
+    mkdir(resultsDir);
+    
+    resultsFile = fullfile(resultsDir, 'testResults.xml');
+    runner.addPlugin(XMLPlugin.producingJUnitFormat(resultsFile));
+   
+    coverageFile = fullfile(resultsDir, 'cobertura.xml');
+    runner.addPlugin(CodeCoveragePlugin.forFolder(src,...
+        'Producing', CoberturaFormat(coverageFile)));
+ 
+    results = runner.run(suite) 
+catch e
+    disp(getReport(e,'extended'));
+    exit(1);
+end
+quit('force');
